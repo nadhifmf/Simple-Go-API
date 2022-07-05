2977369b6855105c4bef9cd41d84a206aff5bcfe
nmfauzan <nadhifmmff@gmail.com>
2022-07-05 16:28:46 +0700

    First simple todo app added

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
-	ID			string
-	Item 		string
-	Completed	bool
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
 var todos = []todo{
-	{ID: "1", Item: "Clean Room", Completed: false}
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Telegram", Completed: false},
+	{ID: "3", Item: "Do Laundry", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9011")
 }
